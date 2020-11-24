c4ec1a5d43235d7ce3ed0c898f5c4690353e4dc3
Aditya Divyang <aditya.divyang69@gmail.com>
2020-11-24 11:55:10 +0530

    added some css to the cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,22 +2,50 @@ import React from 'react';
 import './Card.css';
 var XMLParser = require('react-xml-parser');
 
+// const Card = (props) => {
+// 	var xml = new XMLParser().parseFromString(props.genre);
+//     var valuing = xml.getElementsByTagName('div')[xml.getElementsByTagName('div').length-1].getElementsByTagName('ul')[0].getElementsByTagName('li');
+// 	return(
+// 		<article className="bg-white center card-dimensions ba b--black-10 mv4 hotrodchanges">
+// 			<div className="">
+// 				<h1 className="f6 ttu tracked">{props.id}</h1>
+// 			</div>
+// 				<img src={props.imageurl} className="db" style={{"height":"50%", "width":"50%"}} alt="Cat yawning" />
+// 			<div className="">
+// 				<p className="" >genre</p>
+// 				{valuing.map((data, i)=>{return(<span genrekey={i} className="link dim lh-title">{data.value}</span>)})}
+// 			</div>
+// 		</article>
+// 	)
+// }
+
+// export default Card;
+
 const Card = (props) => {
 	var xml = new XMLParser().parseFromString(props.genre);
     var valuing = xml.getElementsByTagName('div')[xml.getElementsByTagName('div').length-1].getElementsByTagName('ul')[0].getElementsByTagName('li');
 	return(
-		<article className="bg-white center card-dimensions ba b--black-10 mv4 hotrodchanges">
+		<article className="cardCss">
 			<div className="">
-				<h1 className="f6 ttu tracked">{props.id}</h1>
+				<h1 className="cardTitle">{props.id}</h1>
 			</div>
-				<img src={props.imageurl} className="db" style={{"height":"50%", "width":"50%"}} alt="Cat yawning" />
-			<div className="">
-				<p className="" >genre</p>
-				{valuing.map((data, i)=>{return(<span genrekey={i} className="link dim lh-title">{data.value}</span>)})}
+				<img src={props.imageurl} className="" style={{"height":"50%", "width":"50%"}} alt="{props.key}" />
+			<p className="genreName" >genre</p>
+			<div className="genreDiv">
+				{valuing.map((data, i)=>{return(<span genrekey={i} className="genreTitles">{data.value}</span>)})}
 			</div>
 		</article>
 	)
 }
 
 export default Card;
+
+
+
+
+
+
+
+
+
 
